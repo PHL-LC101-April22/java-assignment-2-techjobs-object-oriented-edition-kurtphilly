ca518eda56205eb0e67409f88a2393ed6bd4698c
Kurtphilly <kurt@none>
2022-08-30 08:45:53 -0400

    Work in progress.

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,9 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+//import org.launchcode.techjobs.oo.Job;
 
 /**
  * Created by LaunchCode
@@ -9,4 +11,13 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    @Test
+    public void testSettingJobID() {
+
+        // {data-type} {var-name} = {value};
+        String s1 = new String();
+//        Job job = new Job();
+    }
+
 }
+
